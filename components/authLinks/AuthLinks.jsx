7539ab8daab35cbae98f5569a4eb14e815e30fa1
amirmammadov <amirmammado@gmail.com>
2024-02-02 10:22:23 +0400

    responsive design was added to navbar

--- components/authLinks/AuthLinks.jsx
+++ components/authLinks/AuthLinks.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 import Link from "next/link";
 
 import styles from "./authLinks.module.css";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+
   const status = "notauth";
 
   return (
@@ -13,9 +17,31 @@ const AuthLinks = () => {
         <Link href="/login">Login</Link>
       ) : (
         <>
-          <Link href="/write">Write</Link>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
           <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauth" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
