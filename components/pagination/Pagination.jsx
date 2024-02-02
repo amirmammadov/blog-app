d096b778917e399dc022459ccdc6f4cf0cba2208
amirmammadov <amirmammado@gmail.com>
2024-02-02 21:47:46 +0400

    footer was done

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import styles from "./pagination.module.css";
 
 const Pagination = () => {
-  return <div className={styles.container}>pagination</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  );
 };
 
 export default Pagination;
