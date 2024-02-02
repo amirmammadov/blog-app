9cd0aba1e1f2cd1426eaffe86310f64a23da32d6
amirmammadov <amirmammado@gmail.com>
2024-02-02 21:16:04 +0400

    menu part was completed

--- components/menu/Menu.jsx
+++ components/menu/Menu.jsx
@@ -1,37 +1,20 @@
 import React from "react";
-
-import Link from "next/link";
-import Image from "next/image";
-
 import styles from "./menu.module.css";
+import MenuCategories from "../menucategories/MenuCategories";
+import MenuPosts from "../menuposts/MenuPosts";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/p1.jpeg"
-              alt="post"
-              layout="fill"
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor, sit amet.</h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}> - 02.02.2024</span>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
   );
 };
